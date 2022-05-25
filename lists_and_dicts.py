5e017ada17680fceede3d70866ec843ca2d27cd5
Carlos Castaño <ccastanourrego@gmail.com>
2022-05-25 12:02:51 -0500

    Listas y diccionarios anidados

---
+++ lists_and_dicts.py
@@ -0,0 +1,27 @@
+def run():
+    my_list = [1, 'Hello', True, 4.5]
+    my_dict = {'firstname': 'Facundo', 'lastname': 'Garcia'}
+
+    super_list = [
+        {'firstname': 'Facundo', 'lastname': 'Garcia'},
+        {'firstname': 'Miguel', 'lastname': 'Torres'},
+        {'firstname': 'Pepe', 'lastname': 'Rodelo'},
+        {'firstname': 'Susana', 'lastname': 'Martinez'},
+        {'firstname': 'José', 'lastname': 'Garcia'},
+    ]
+
+    super_dict = {
+        'natural_nums': [1, 2, 3, 4, 5],
+        'integer_nums': [-1, -2 , 0, 1, 2],
+        'floating_nums': [1.1, 4.5, 6.43],
+    }
+
+    for key, value in super_dict.items():
+        print(key, '-', value)
+
+    for i in super_list:
+        print(i["firstname"], i["lastname"])
+
+
+if __name__ == '__main__':
+    run()
